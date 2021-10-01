03b55529c3706f63e7e6602e468c1a905546ec2e
carlyls <63876123+carlyls@users.noreply.github.com>
2021-10-01 15:17:22 -0400

    Update Simulation_Data.R

--- Simulation_Data.R
+++ Simulation_Data.R
@@ -2,6 +2,16 @@
 
 ## Plan documentation: https://docs.google.com/document/d/1t2ld8Vj1xxdm9FwklDUiOgLprDir_1LmELZpoYLGmcA/edit
 
+# smaller difference
+n.trial <- 1000
+n.obs <- 2000
+reg.coefs <- c(-.5,.7,.4,.2,.4,.4,.2,.2,.2) #check this
+mu.trial <- c(1,1)
+mu.obs <- c(0,.5)
+Sigma.trial <- Sigma.obs <- cbind(c(.5,0),c(0,.5))
+p.trial <- 0.5
+p.obs <- 0.2
+
 DataGen <- function(n.trial, n.obs, reg.coefs, mu.trial, mu.obs,
                     Sigma.trial, Sigma.obs, p.trial, p.obs) {
   
@@ -21,7 +31,7 @@ DataGen <- function(n.trial, n.obs, reg.coefs, mu.trial, mu.obs,
   X3.obs <- sample(0:1, size=n.obs, prob=c(1-p.obs,p.obs), replace=TRUE)
   
   ## Compute treatment assignment probabilities for the observational sample
-  lin_pred <- -1/2 + X1.obs/2 + .4*X2.obs + .4*X2.obs^2
+  lin_pred <- -1/2 + X1.obs/2 + .4*X2.obs + .5X1.obs^2 + .4*X2.obs^2
   pp <- exp(lin_pred)/(1 + exp(lin_pred))
   
   ## Generate treatment assignments for each sample
@@ -35,12 +45,14 @@ DataGen <- function(n.trial, n.obs, reg.coefs, mu.trial, mu.obs,
   outcome.prob.trial <- rep(0.0, n.trial)
   outcome.prob.obs <- rep(0.0, n.obs)
   for(k in 1:n.trial) {
-    dd <- c(1, A.trial[k], X1.trial[k], X2.trial[k], X3.trial[k], (X1.trial[k])^2, A.trial[k]*X2.trial[k], A.trial[k]*X3.trial[k])
+    dd <- c(1, A.trial[k], X1.trial[k], X2.trial[k], X3.trial[k], (X1.trial[k])^2, (X2.trial[k])^2,
+            A.trial[k]*X2.trial[k], A.trial[k]*X3.trial[k])
     lin_pred <- sum(reg.coefs*dd)
     outcome.prob.trial[k] <- exp(lin_pred)/(1 + exp(lin_pred))
   }
   for(k in 1:n.obs) {
-    dd <- c(1, A.obs[k], X1.obs[k], X2.obs[k], X3.obs[k], (X1.obs[k])^2, A.obs[k]*X2.obs[k], A.obs[k]*X3.obs[k])
+    dd <- c(1, A.obs[k], X1.obs[k], X2.obs[k], X3.obs[k], (X1.obs[k])^2, (X2.obs[k])^2, 
+            A.obs[k]*X2.obs[k], A.obs[k]*X3.obs[k])
     lin_pred <- sum(reg.coefs*dd)
     outcome.prob.obs[k] <- exp(lin_pred)/(1 + exp(lin_pred))
   }
